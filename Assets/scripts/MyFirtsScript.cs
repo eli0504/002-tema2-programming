b795b99a6fe8f208b5f49634651705cccd310f4c
eli0504 <2003ecc@gmail.com>
2022-10-10 10:45:45 +0200

    practica-scripts

---
+++ Assets/scripts/MyFirtsScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirtsScript : MonoBehaviour
+{
+    /* 
+     4 variables with the player information 
+    */
+
+    // This variable saves the player's ages
+    public int playerAge = 33;
+
+    // This variable saves the player's speed
+    public float playerSpeed = 3.125f;
+    public string playerName = "Eli Caparros";
+    public bool gameOver = false;
+
+}
+   
